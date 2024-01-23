cda19b991a66233537e974b0911613bb8a119b70
soqohm <soqohm@yandex.ru>
2024-01-23 23:55:05 +0500

    *
    *

---
+++ 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Ясный_код
+{
+    /*
+    
+    parents1 - receiverParents
+    // родители объекта приемника
+
+    parents2 - sourceParents
+    // родители объекта источника
+
+    amount - totalHierarchyAmount
+    // количество объекта с учетом количеств родительских объектов вверх по дереву до объекта головы 
+
+    changes1 - mainReglamentChanges
+    // список объектов (извещений об изменении), полученных по алгоритму основного регламента
+
+    changes2 - tempReglamentChanges
+    // список объектов (извещений об изменении), полученных по алгоритму временного регламента
+
+    changes3 - migrationChanges
+    // список объектов (извещений об изменении), полученных по результатам миграции данных
+
+    key1 - selectedValueKey
+    // переменная ключ для доступа к основному полю диалога
+
+    key2 - changedValueKey
+    // переменная ключ для доступа к дополнительному полю диалога (которое определяется автоматически в зависимости от значения selectedValueKey)
+
+    key3 - hiddenValueKey
+    // переменная ключ для доступа к дополнительному полю диалога (которое в большинстве случаев скрыто)
+
+
+
+    */
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("! Ясный код")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("! Ясный код")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9cf83883-f7b2-4785-b4d0-9e45065e7ff5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
