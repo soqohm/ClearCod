ce9d631f48ecf0198d26035ee10a6e3200caa461
soqohm <soqohm@yandex.ru>
2024-01-26 23:43:50 +0500

    *
    *

---
+++ 2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Ясный_код
+{
+    /*
+    
+
+    6.1
+
+
+    abstractParentDefaultValue
+
+    containsDifferentAbstractTypes
+
+    loadedWeights
+
+    multiselectBox
+
+    documentExternalFileLinks
+
+
+    6.2
+
+
+    numHashFunctions
+
+    bloomFilter
+
+    recursiveLoadHierarchyLinks
+
+    structNodeComparer
+
+    multiselectListForm
+
+
+    6.3
+
+
+    factoryName
+
+    tflexPluginButton
+
+    deletedVariables
+
+
+    6.4
+
+
+    multiselectListFromListForm - multiselectListForm
+
+    additionalHierarchyData - hierarchyData
+
+    fileLocalPathFromFileReference - workFolder
+
+    obj - startDocument
+
+    technologicalProcessesReference - techReference
+    
+
+    */
+}
