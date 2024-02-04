f80bec4ea27b4516be51dca427b8cf0f17c0ff5c
soqohm <soqohm@yandex.ru>
2024-02-04 23:22:14 +0500

    *
    *

---
+++ 5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Ясный_код
+{
+    /*
+       
+
+    // в работе
+
+
+    */
+}
