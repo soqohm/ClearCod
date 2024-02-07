b330c186e2cf6ed585f430f0a6201cf974583255
soqohm <soqohm@yandex.ru>
2024-02-07 23:21:45 +0500

    *
    *

---
+++ 6.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Ясный_код
+{
+    /*
+       
+
+    1.
+
+
+    Macro - ChangeTypesSelectedObjects
+    // метод, который запускает цепочку функций по смене типа у выделенных в программе объектов
+
+
+    2.
+
+
+    IsWorked - ContainsDifferentAbstractTypes
+    // метод, принимающий список объектов и возвращающий true, если имеются объекты с разными родительскими абстрактными типами
+
+
+    3.
+
+
+    GetAbstractParentDefaultValue - GetAbstractTypeName
+    // по сути, слова Parent и Default лишние, метод возвращает имя абстракного типа у объекта
+
+
+    4.
+
+
+    Macro - CopyDataFromSourceToReceiver
+    // метод, который запускает цепочку функций по заимствованию данных от одного объекта к другому
+
+
+    5.
+
+
+    On - ChangeOn
+    // метод у специального класса для смены стадии объекта, меняет стадию объекта с текущей на вспомогательную 
+
+
+    6.
+
+
+    Off - ChangeOff
+    // метод у специального класса для смены стадии объекта, возвращает стадию объекта с вспомогательной на ту, которая была
+
+
+    7.
+
+
+    GetConfigSettings - SetConfigSettings
+    // метод, который применяет настройки конфигурирования, поэтому ему корректней называться set, а не get
+
+
+    8.
+
+
+    GetSortedIndex - GetBomSectionPriority
+    // метод, который принимает тип объекта, и возвращает его приоритет расположения в спецификации, используется для сортировки объектов
+
+
+    9.
+
+
+    GetDialog - CreateDialogForSetNewTypes
+    // метод, который возвращает специальный диалог для смены типа объектов
+
+
+    10.
+
+
+    GetUser - GetUserByFullName
+    // получает пользователя по полному имени
+
+
+    11.
+
+
+    DeleteVariables - RecursiveDeleteVariables
+    // метод, который удаляет переданные переменные не только у текущего объекта, но и у всех его дочерних
+
+
+    12.
+
+
+    GetResultArray - GetArrayToDisplayInExcel
+    // возвращает массив данных для последующей передачи в Excel-отчет
+
+
+    */
+}
