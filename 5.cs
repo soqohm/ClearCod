1ef37db5b1dceb44eba44c0a7ef2c850e0d359f2
soqohm <soqohm@yandex.ru>
2024-03-09 12:29:44 +0500

    *
    *

--- 5.cs
+++ 5.cs
@@ -44,22 +44,37 @@ namespace __Ясный_код
     стало: DepthWeights
 
 
-    6.
+    6. Использование Number как "Обозначение" (не Designation, не Denotation) 
 
 
-    //
+    public string GetNumber()
+    {
+        return Source[СправочникЭлектроннаяСтруктура.Параметр.Обозначение].GetString();
+    }
 
 
-    7.
+    7. Использование Recursive - т.е. на всю иерархию вниз
 
 
-    //
+    public static void RecursiveDeleteVariables()
+
+    public static IEnumerable<Fragment3D> RecursiveLoadHierarchy(this Document document, HierarchyData data)
+
+    public static class RecursiveLoadChilds
 
 
-    8.
+    8. Использование Load вместо Get, где идет обращение к серверу
 
 
-    //
+    public Reference LoadReference(string referenceName)
+
+    public List<ReferenceObject> LoadTechs(params string[] namesOfTechTypes)
+
+    public List<ReferenceObject> LoadFiles()
+
+    public IEnumerable<NomenclatureHierarchyLink> LoadParents()
+
+    public IEnumerable<NomenclatureHierarchyLink> LoadChilds()
 
 
     9.
